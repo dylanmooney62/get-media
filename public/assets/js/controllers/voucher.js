dbba43615f5867d63db95ce876e58633ba9b4316
Dylan Mooney <dylanmooney62@gmail.com>
2019-10-19 08:56:53 +0100

    added voucher controller, displayed list of vouchers

---
+++ public/assets/js/controllers/voucher.js
@@ -0,0 +1,10 @@
+export default function($scope) {
+  $scope.vouchers = [
+    { id: 0, title: 'Apple Store Credit', credit: 10, cost: 10 },
+    { id: 1, title: 'Apple Store Credit', credit: 25, cost: 25 },
+    { id: 2, title: 'Apple Store Credit', credit: 30, cost: 30 },
+    { id: 3, title: 'Apple Store Credit', credit: 50, cost: 50 },
+    { id: 4, title: 'Apple Store Credit', credit: 75, cost: 75 },
+    { id: 5, title: 'Apple Store Credit', credit: 100, cost: 100 },
+  ];
+}
--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,5 +1,7 @@
 import navCtrl from './controllers/nav.js';
+import voucherCtrl from './controllers/voucher.js';
 
 var app = angular.module('app', []);
 
 app.controller('navCtrl', navCtrl);
+app.controller('voucherCtrl', voucherCtrl);
