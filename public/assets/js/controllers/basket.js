acf55e72df512697cb27b9ec23e48630371759a6
Dylan Mooney <dylanmooney62@gmail.com>
2019-10-20 17:47:59 +0100

    added voucher png files, updated images folder structure

--- public/assets/js/controllers/basket.js
+++ public/assets/js/controllers/basket.js
@@ -9,15 +9,15 @@ export default function($scope, $rootScope) {
     // check if item exists in basket
     const existingItem = $scope.basket.find(({ id }) => id === itemToAdd.id);
 
-    // if already exists update quantity
     if (existingItem) {
+      // creates copy of array and adds + 1 to quantity of existing item
       $scope.basket = $scope.basket.map((basketItem) =>
         basketItem.id === existingItem.id
           ? { ...basketItem, quantity: basketItem.quantity + 1 }
           : basketItem,
       );
     } else {
-      // if item doesn't exist add item to basket with quantity 1
+      // if item doesn't exist create copy of array and add itemToAdd with quanity 1
       $scope.basket = [...$scope.basket, { ...itemToAdd, quantity: 1 }];
     }
   };
