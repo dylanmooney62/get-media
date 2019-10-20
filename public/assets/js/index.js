17a0fca902a7e658f18225c495031a733777ec93
Dylan Mooney <dylanmooney62@gmail.com>
2019-10-20 18:37:32 +0100

    renamed voucher controller to shop, basket now shows unique image

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,9 +1,9 @@
 import navCtrl from './controllers/nav.js';
-import voucherCtrl from './controllers/voucher.js';
+import shopCtrl from './controllers/shop.js';
 import basketCtrl from './controllers/basket.js';
 
 var app = angular.module('app', []);
 
 app.controller('navCtrl', navCtrl);
-app.controller('voucherCtrl', voucherCtrl);
+app.controller('shopCtrl', shopCtrl);
 app.controller('basketCtrl', basketCtrl);
