4203460124cff57f35989f6e5434a1c2e6b4e6fa
Dylan Mooney <dylanmooney62@gmail.com>
2019-10-21 02:22:38 +0100

    made basket responsive

--- public/assets/js/controllers/nav.js
+++ public/assets/js/controllers/nav.js
@@ -1,5 +1,5 @@
 export default function($scope, $rootScope) {
-  $scope.showBasket = false;
+  $scope.showBasket = true;
 
   $scope.$on('$includeContentLoaded', function(event, templateName) {
     const nav = document.querySelector('.sidenav');
